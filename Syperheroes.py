12ae79d29ef50c8d58b2e81f69dba297f00d6b7f
Марк <fenster.mark@yandex.ru>
2023-02-05 21:29:52 +0200

    Comments deleted

--- Syperheroes.py
+++ Syperheroes.py
@@ -11,7 +11,6 @@ class SuperHeroes:
         self.full_path = self.main_resourse + self.additional_resourse
     
     def get_info(self):
-        # self.data_format = data_format
         response = requests.get(self.full_path)
         return response.json()
     
@@ -32,24 +31,3 @@ if __name__ == '__main__':
 
     intell = SuperHeroes('https://akabab.github.io/superhero-api/api', '/all.json', ['Hulk', 'Captain America', 'Thanos'])
     print(intell.the_cleverest())
-
-# WRITING INTO FILE
-# with open('all_super_heroes.txt', 'a', encoding='utf-8') as file:
-#     for item in request.get_info():
-#         file.write(str(item) + '\n')
-
-###############################################################################
-
-# target_dict = {}
-
-# for item in request:
-#     if item['name'] in intell.superheroes_list:
-#         target_dict[item['name']] = item['powerstats']['intelligence']
-
-# for key, value in target_dict.items():
-#     if value == max(list(target_dict.values())):
-#         result = key
-
-# print(result)
-# print(target_dict)
-
